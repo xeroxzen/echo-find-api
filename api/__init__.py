6e6ef27e99934568524c9332d29df62acc977450
Google Jr <andilembele020@gmail.com>
2025-08-09 03:16:31 +0200

    Added initial API structure with modules for core functionality, including database, models, routers, and services.

---
+++ api/__init__.py
@@ -0,0 +1,1 @@
+# EchoFind API Package
---
+++ api/models/__init__.py
@@ -0,0 +1,13 @@
+from .upload import UploadResponse, AudioFile
+from .search import SearchRequest, SearchResponse, SearchResult
+from .playback import PlaybackRequest, PlaybackResponse
+
+__all__ = [
+    "UploadResponse",
+    "AudioFile", 
+    "SearchRequest",
+    "SearchResponse",
+    "SearchResult",
+    "PlaybackRequest",
+    "PlaybackResponse"
+]
